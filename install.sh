c480647cf52f1479f1a20c7853eade037330becc
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-10-25 17:20:03 -0400

    Added font installation for i3

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 sudo apt-get install git
 sudo apt-get install ssh
+sudo apt-get install i3blocks
+sudo apt-get install feh
 # make symbolic links
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.i3 ~/.i3
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sL ~/dotfiles/.i3/ ~/.i3/
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+# configure i3
+# install San Francisco font
+mkdir ~/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
+
+wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip; unzip v*.zip; rm v*.zip; mv Font-Awesome*/fonts/*.ttf ~/.fonts/; rm -r Font-Awesome* 
 
