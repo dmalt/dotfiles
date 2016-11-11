01681b37f39dabf303ba5035cf86f27f74b64b6a
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-11-11 00:13:30 -0500

    Removed all crap from install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
-sudo apt-get install ssh
-sudo apt-get install i3blocks
-sudo apt-get install feh
-sudo apt-get install conky
-
-# make symbolic links
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-if ! [[ -f ~/.i3 ]]; then
-    ln -s ~/dotfiles/.i3/ ~/.i3
-fi
 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.conkyrc ~/.conkyrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/pure.zsh-theme ~/.oh-my-zsh/themes/pure.zsh-theme
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
 
-# configure i3
-# install San Francisco font
-mkdir ~/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
-
-wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip; unzip v*.zip; rm v*.zip; mv Font-Awesome*/fonts/*.ttf ~/.fonts/; rm -r Font-Awesome* 
-
-#wget https://github.com/DaveDavenport/rofi/releases/download/1.2.0/rofi-1.2.0.tar.gz; 
