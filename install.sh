bdfc028b49471a9166ed9e07862065596bcd2933
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-01-12 18:12:58 +0300

    Moved plugins installation before links

--- install.sh
+++ install.sh
@@ -5,27 +5,28 @@
 # sudo apt-get install conky
 
 # make symbolic links
-if ! [[ -d ~/.i3 ]]; then
-    ln -sv ~/dotfiles/.i3/ ~/.i3
-fi
-
-ln -sfv ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sfv ~/dotfiles/.zshrc ~/.zshrc
-ln -sfv ~/dotfiles/.vimrc ~/.vimrc
-ln -sfv ~/dotfiles/.conkyrc ~/.conkyrc
-ln -sfv ~/dotfiles/.zshrc ~/.zshrc
-ln -sfv ~/dotfiles/pure_dmalt.zsh-theme ~/.oh-my-zsh/themes/pure_dmalt.zsh-theme
-ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sfv $HOME/dotfiles/.xinputrc $HOME/.xinputrc
-ln -sfv $HOME/dotfiles/.vimrc $HOME/.config/nvim/init.vim
-
-
 bash -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+if ! [[ -d $HOME/.i3 ]]; then
+    ln -sv $HOME/dotfiles/.i3/ $HOME/.i3
+fi
+
+ln -sfv $HOME/dotfiles/.gitconfig             $HOME/.gitconfig
+ln -sfv $HOME/dotfiles/.zshrc                 $HOME/.zshrc
+ln -sfv $HOME/dotfiles/.vimrc                 $HOME/.vimrc
+ln -sfv $HOME/dotfiles/.conkyrc               $HOME/.conkyrc
+ln -sfv $HOME/dotfiles/.zshrc                 $HOME/.zshrc
+ln -sfv $HOME/dotfiles/pure_dmalt.zsh-theme   $HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme
+ln -sfv $HOME/dotfiles/.tmux.conf             $HOME/.tmux.conf
+ln -sfv $HOME/dotfiles/.xinputrc              $HOME/.xinputrc
+ln -sfv $HOME/dotfiles/.vimrc                 $HOME/.config/nvim/init.vim
+
+
 # configure i3
 # install San Francisco font
-# mkdir ~/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
+# mkdir $HOME/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
 
 # wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip; unzip v*.zip; rm v*.zip; mv Font-Awesome*/fonts/*.ttf ~/.fonts/; rm -r Font-Awesome* 
 
