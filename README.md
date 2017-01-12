ceeeeb64fc3953c99953a5b8a54f8f49a8973f7f
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-01-12 15:28:14 +0300

    Added oh-my-zsh and zsh-syntax-highlighting to install.sh

--- README.md
+++ README.md
@@ -14,36 +14,11 @@ semiautomatic tool to apply all the custom setups on a completely new machine
 | Version control | git      | .gitconfig        |
 | System monitor  | conky    | .conkyrc          |
 
-# Manual tweaks
-
-### ZSHELL
-
-#### Plugins to be installed manually
-1. oh-my-zsh
-    ```bash
-    bash -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    ```
-
-2. zsh-syntax-highlighting
-    ```bash
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    ```
-
-#### Theme
-I use oh-my-zsh for zshell customization. I really like pure theme
-but I wanted to add timestamp to the right of the screen so I modified it a bit and
-put my version in this repo. To make it available create a symlink inside .oh-my-zsh/themes folder like this:
+# Installation
 
 ```bash
-ln -s ~/dotfiles/pure_dmalt.zsh-theme ~/.oh-my-zsh/themes/pure_dmalt.zsh-theme
+git clone git@github.com:dmalt/dotfiles.git
+cd dotfiles
+./install.sh
 ```
-
-After that you can activate it adding 
-
-```bash
-ZSH_THEME="pure_dmalt"
-```
-
-to your .zshrc file
-
 
--- install.sh
+++ install.sh
@@ -5,20 +5,23 @@
 # sudo apt-get install conky
 
 # make symbolic links
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-if ! [[ -f ~/.i3 ]]; then
-    ln -s ~/dotfiles/.i3/ ~/.i3
+if ! [[ -d ~/.i3 ]]; then
+    ln -sv ~/dotfiles/.i3/ ~/.i3
 fi
 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.conkyrc ~/.conkyrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/pure_dmalt.zsh-theme ~/.oh-my-zsh/themes/pure_dmalt.zsh-theme
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s $HOME/dotfiles/.xinputrc $HOME/.xinputrc
+ln -sfv ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sfv ~/dotfiles/.zshrc ~/.zshrc
+ln -sfv ~/dotfiles/.vimrc ~/.vimrc
+ln -sfv ~/dotfiles/.conkyrc ~/.conkyrc
+ln -sfv ~/dotfiles/.zshrc ~/.zshrc
+ln -sfv ~/dotfiles/pure_dmalt.zsh-theme ~/.oh-my-zsh/themes/pure_dmalt.zsh-theme
+ln -sfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfv $HOME/dotfiles/.xinputrc $HOME/.xinputrc
 
 
+bash -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # configure i3
 # install San Francisco font
 # mkdir ~/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
