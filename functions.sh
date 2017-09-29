f6c2da13c4242ddabc21e9f629f6ed965f09ebad
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-09-29 13:58:35 +0300

    add showbat and toggle_touchpad

--- functions.sh
+++ functions.sh
@@ -5,6 +5,20 @@ fwd_jupyter () {
 }
 
 
-killname () {
-    kill `pgrep $1`
+
+
+showbat () {
+    upower -i `upower -e | grep BAT` | grep percentage
+    upower -i `upower -e | grep BAT` | grep 'time to full'
 }
+
+toggle_touchpad() {
+    tpad_id=`xinput | grep Touchpad | sed -E 's/.*id=([0-9]+).*/\1/'`
+    state=`xinput list-props $tpad_id | grep 'Device Enabled' | sed -E 's/.*([0,1])$/\1/'`
+    if [[ $state -eq 1 ]]; then 
+        xinput --disable $tpad_id
+    else
+        xinput --enable $tpad_id
+    fi
+}
+
