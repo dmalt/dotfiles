8a073de3f6df4beb1bada2cfd08b719b0fac1828
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-10-20 16:21:44 +0300

    mv configs for i3, i3blocks, rofi and conky to ~/.config

--- make_links.sh
+++ make_links.sh
@@ -1,15 +1,13 @@
-if ! [[ -d $HOME/.i3 ]]; then
-    ln -sv $HOME/dotfiles/.i3/ $HOME/.config/i3
+if ! [[ -d $HOME/.config/i3 ]]; then
+    ln -sv $HOME/dotfiles/.i3 $HOME/.config/i3
 fi
 
-if ! [[ -d $HOME/.config/rofi ]]; then
-    ln -sv $HOME/dotfiles/rofi/ $HOME/.config/rofi
-fi
+mkdir -p $HOME/.config/rofi && ln -sfv $HOME/dotfiles/rofi/config $HOME/.config/rofi/config
+mkdir -p $HOME/.config/conky && ln -sfv $HOME/dotfiles/.conkyrc $HOME/.config/conky/conky.conf
 
 ln -sfv $HOME/dotfiles/.gitconfig             $HOME/.gitconfig
 ln -sfv $HOME/dotfiles/.zshrc                 $HOME/.zshrc
 ln -sfv $HOME/dotfiles/.vimrc                 $HOME/.vimrc
-ln -sfv $HOME/dotfiles/.conkyrc               $HOME/.conkyrc
 ln -sfv $HOME/dotfiles/.zshrc                 $HOME/.zshrc
 ln -sfv $HOME/dotfiles/pure_dmalt.zsh-theme   $HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme
 ln -sfv $HOME/dotfiles/.tmux.conf             $HOME/.tmux.conf
