68a386ca71d6768807063e060d88bc3bb7f37204
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-10-20 15:52:54 +0300

    move out links

--- install.sh
+++ install.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
-# sudo apt-get install ssh
-# sudo apt-get install i3blocks
-# sudo apt-get install feh
-# sudo apt-get install conky
 
-# make symbolic links
+# ---------- install oh-my-zsh and plugins ----------- #
 bash -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# ---------------------------------------------------- #
 
 # Install Vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-if ! [[ -d $HOME/.i3 ]]; then
-    ln -sv $HOME/dotfiles/.i3/ $HOME/.i3
-fi
-
-if ! [[ -d $HOME/.config/rofi ]]; then
-    ln -sv $HOME/dotfiles/rofi/ $HOME/.config/rofi
-fi
-
-ln -sfv $HOME/dotfiles/.gitconfig             $HOME/.gitconfig
-ln -sfv $HOME/dotfiles/.zshrc                 $HOME/.zshrc
-ln -sfv $HOME/dotfiles/.vimrc                 $HOME/.vimrc
-ln -sfv $HOME/dotfiles/.conkyrc               $HOME/.conkyrc
-ln -sfv $HOME/dotfiles/.zshrc                 $HOME/.zshrc
-ln -sfv $HOME/dotfiles/pure_dmalt.zsh-theme   $HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme
-ln -sfv $HOME/dotfiles/.tmux.conf             $HOME/.tmux.conf
-ln -sfv $HOME/dotfiles/.xinputrc              $HOME/.xinputrc
-ln -sfv $HOME/dotfiles/.vimrc                 $HOME/.config/nvim/init.vim
-
+# make symbolic links
+./make_links.sh
 
 # configure i3
 # install San Francisco font
-# mkdir $HOME/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
+ mkdir $HOME/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
 
-# wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip; unzip v*.zip; rm v*.zip; mv Font-Awesome*/fonts/*.ttf ~/.fonts/; rm -r Font-Awesome* 
+ wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip; unzip v*.zip; rm v*.zip; mv Font-Awesome*/fonts/*.ttf ~/.fonts/; rm -r Font-Awesome* 
 
-#wget https://github.com/DaveDavenport/rofi/releases/download/1.2.0/rofi-1.2.0.tar.gz; 
+# wget https://github.com/DaveDavenport/rofi/releases/download/1.2.0/rofi-1.2.0.tar.gz; 
