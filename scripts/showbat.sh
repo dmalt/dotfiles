fa97453ad90027cd55886ddaa9893d64e200e7eb
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-10-20 15:53:23 +0300

    add script for i3blocks to show  battery status

--- scripts/showbat.sh
+++ scripts/showbat.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/zsh
 
 bname=$(upower -e | grep BAT)
-percentage=`upower -i $bname | grep percentage | sed -E 's/.*\s([0-9]{1,2}%)/\1/'`
+percentage=`upower -i $bname | grep percentage | sed -E 's/.*\s([0-9]{1,3}%)/\1/'`
 time2full=`upower -i $bname | grep 'time to full' | sed -E 's/.*\s([0-9\.]+\s[a-z])/\1/'`
 energyrate=`upower -i $bname | grep 'energy-rate' | sed -E 's/.*\s([0-9\.]+\sW)/\1/'`
 time2empty=`upower -i $bname | grep 'time to empty' | sed -E 's/.*\s([0-9\.]+\s[a-z])/\1/'`
-state=`upower -i $bname | grep  'state' | sed -E 's/.*\s(charging|discharging)/\1/'`
+state=`upower -i $bname | grep  'state' | sed -E 's/.*\s(charging|discharging|fully-charged)/\1/'`
 
 
 if [[ "$state" == "discharging" ]]; then
@@ -15,6 +15,8 @@ elif [[ "$state" == "charging" ]]; then
         ttf_str=(\("$time2full" to full\))
     fi
     status_str=("$percentage" $ttf_str; charging rate: "$energyrate")
+elif [[ "$state" == "fully-charged" ]]; then
+    status_str=("$percentage" $ttf_str; "fully-charged")
 fi
 
 echo $status_str
