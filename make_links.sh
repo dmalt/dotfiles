eada0474a057ebc7cf931d28bde170b78ec4e228
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-06 18:40:41 +0300

    --amend

--- make_links.sh
+++ make_links.sh
@@ -8,10 +8,9 @@ mkdir -p $HOME/.config/conky && ln -sfv $DOTFILES/.conkyrc $HOME/.config/conky/c
 
 ln -sfv $DOTFILES/gitconfig             $HOME/.gitconfig
 ln -sfv $DOTFILES/zshrc                 $HOME/.zshrc
-ln -sfv $DOTFILES/.vimrc                 $HOME/.vimrc
-ln -sfv $DOTFILES/.zshrc                 $HOME/.zshrc
+ln -sfv $DOTFILES/vimrc                 $HOME/.vimrc
 ln -sfv $DOTFILES/pure_dmalt.zsh-theme   $HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme
-ln -sfv $DOTFILES/.tmux.conf             $HOME/.tmux.conf
-ln -sfv $DOTFILES/.xinputrc              $HOME/.xinputrc
-ln -sfv $DOTFILES/.vimrc                 $HOME/.config/nvim/init.vim
-ln -sfv $DOTFILES/.zlogin                $HOME/.zlogin
+ln -sfv $DOTFILES/tmux.conf             $HOME/.tmux.conf
+ln -sfv $DOTFILES/xinputrc              $HOME/.xinputrc
+ln -sfv $DOTFILES/vimrc                 $HOME/.config/nvim/init.vim
+ln -sfv $DOTFILES/zlogin                $HOME/.zlogin
