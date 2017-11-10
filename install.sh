bc898de5de0224af50385c2b3f96c785feb59016
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-11-10 15:54:16 +0300

    change ; to &&

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.v
 
 # configure i3
 # install San Francisco font
- mkdir $HOME/.fonts; wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip; unzip master.zip; rm master.zip; mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/; rm -r YosemiteSanFranciscoFont-master
+ mkdir $HOME/.fonts && wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip && unzip master.zip && rm master.zip && mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/ && rm -r YosemiteSanFranciscoFont-master
 
- wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip; unzip v*.zip; rm v*.zip; mv Font-Awesome*/fonts/*.ttf ~/.fonts/; rm -r Font-Awesome* 
+ wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip && unzip v*.zip && rm v*.zip && mv Font-Awesome*/fonts/*.ttf ~/.local/share/fonts/ && rm -r Font-Awesome* 
 
 # wget https://github.com/DaveDavenport/rofi/releases/download/1.2.0/rofi-1.2.0.tar.gz; 
