925709a9760049e969b3c4e98482c835ed6438f0
Dmitrii Altukhov <dm.altukhov@ya.ru>
2021-03-28 16:27:13 +0300

    Fix error

--- make_links.sh
+++ make_links.sh
@@ -5,22 +5,20 @@ fi
 
 mkdir -p $HOME/.config/rofi && ln -sfv $DOTFILES/rofi/config $HOME/.config/rofi/config
 mkdir -p $HOME/.config/conky && ln -sfv $DOTFILES/conkyrc $HOME/.config/conky/conky.conf
+mkdir -p $HOME/.config/termite && ln -sfv $DOTFILES/termiterc $HOME/.config/termite/config
 
 ln -sfv $DOTFILES/gitconfig             $HOME/.gitconfig
 ln -sfv $DOTFILES/zshrc                 $HOME/.zshrc
 ln -sfv $DOTFILES/pure_dmalt.zsh-theme  $HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme
 ln -sfv $DOTFILES/tmux.conf             $HOME/.tmux.conf
 ln -sfv $DOTFILES/xinputrc              $HOME/.xinputrc
-ln -sfv $DOTFILES/nvim/init.vim         $HOME/.config/nvim/init.vim
 ln -sfv $DOTFILES/nvim/init.vim         $HOME/.vimrc
-ln -sfv $DOTFILES/nvim/plugins.lua      $HOME/.config/nvim/plugins.lua
 ln -sfv $DOTFILES/zlogin                $HOME/.zlogin
-ln -sfv $DOTFILES/termiterc             $HOME/.config/termite/config
 
-if [[ -d $HOME/.config/nvim/ftplugin ]]; then
-    rm -r $HOME/.config/nvim/ftplugin
+if [[ -d $HOME/.config/nvim ]]; then
+    rm -r $HOME/.config/nvim/
 fi
-ln -sfv $DOTFILES/nvim/ftplugin         $HOME/.config/nvim/ftplugin
+ln -sfv $DOTFILES/nvim $HOME/.config/nvim
 
 if [[ -d $HOME/.config/ranger ]]; then
     rm -r $HOME/.config/ranger
