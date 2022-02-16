e5819d99b51c6c3a6fe5788c692397a221ab2e98
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-02-16 18:17:51 +0300

    fix bugs, remove unnecessary stuff

--- make_links.sh
+++ make_links.sh
@@ -1,11 +1,5 @@
 export DOTFILES=$HOME/.dotfiles
-if ! [[ -d $HOME/.config/i3 ]]; then
-    ln -sv $DOTFILES/i3 $HOME/.config/i3
-fi
 
-mkdir -p $HOME/.config/rofi && ln -sfv $DOTFILES/rofi/config $HOME/.config/rofi/config
-mkdir -p $HOME/.config/conky && ln -sfv $DOTFILES/conkyrc $HOME/.config/conky/conky.conf
-mkdir -p $HOME/.config/termite && ln -sfv $DOTFILES/termiterc $HOME/.config/termite/config
 
 ln -sfv $DOTFILES/gitconfig             $HOME/.gitconfig
 ln -sfv $DOTFILES/zshrc                 $HOME/.zshrc
@@ -15,12 +9,15 @@ ln -sfv $DOTFILES/xinputrc              $HOME/.xinputrc
 ln -sfv $DOTFILES/nvim/init.vim         $HOME/.vimrc
 ln -sfv $DOTFILES/zlogin                $HOME/.zlogin
 
-if [[ -d $HOME/.config/nvim ]]; then
-    rm -r $HOME/.config/nvim/
+if [ -L "$HOME/.config/nvim" ] || [ -d "$HOME/.config/nvim" ]
+then
+    rm -r $HOME/.config/nvim
 fi
 ln -sfv $DOTFILES/nvim $HOME/.config/nvim
 
-if [[ -d $HOME/.config/ranger ]]; then
+
+if [ -L "$HOME/.config/ranger" ] || [ -d "$HOME/.config/ranger" ]
+then
     rm -r $HOME/.config/ranger
 fi
-ln -sfv $DOTFILES/ranger                $HOME/.config/ranger
+ln -sfv "$DOTFILES/ranger"              $HOME/.config/ranger
