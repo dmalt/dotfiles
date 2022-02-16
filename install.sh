ee9d808f45dadd047bb84adb470cb1ab645d3813
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-02-16 18:57:05 +0300

    add zsh-autosuggestions install

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-linux_packages=(neovim git wget curl tmux ranger bat ripgrep)
-python_packages=(neovim flake8 jedi)
 
-distro=`lsb_release -i | sed -r 's/Distributor ID:\t([A-Z][a-z]+)/\1/'` 
+# include git in case it's not already installed, i.e. dotfiles were downloaded as zip
+linux_packages=(neovim git wget curl tmux ranger bat python3-pip htop)
+python_packages=(neovim flake8 jedi black)
+
+# ---------- get distribution plugin manager -------- #
+distro=`lsb_release -i | sed -r 's/Distributor ID:\t([A-Z][a-z]+)/\1/'`
 
 if [[ $distro == Arch ]]; then
     installer="pacman -S"
@@ -13,6 +16,8 @@ else
     exit 1
 fi
 
+
+# install packages
 for p in ${linux_packages[@]}; do
     /usr/bin/bash -c "sudo $installer $p"
 done
@@ -23,25 +28,28 @@ done
 
 # ---------- install oh-my-zsh and plugins ----------- #
 bash -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]
+then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]
+then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 # ---------------------------------------------------- #
 
 # Install Vundle for vim
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]
+then
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
 
 # make symbolic links
 ./make_links.sh
 
 # install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-$HOME/.fzf/install
-
-# configure i3
-# install San Francisco font
- mkdir $HOME/.fonts && wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip && unzip master.zip && rm master.zip && mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/ && rm -r YosemiteSanFranciscoFont-master
-
- # install awesome font for i3 icons
- wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.zip && unzip v*.zip && rm v*.zip && mv Font-Awesome*/fonts/*.ttf ~/.local/share/fonts/ && rm -r Font-Awesome* 
-
-# wget https://github.com/DaveDavenport/rofi/releases/download/1.2.0/rofi-1.2.0.tar.gz; 
+if [ ! -d $HOME/.fzf ]
+then
+    git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+    $HOME/.fzf/install
+fi
