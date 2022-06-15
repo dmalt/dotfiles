08fd7ca7b319303b488287252a4b78c6a99ba1e6
Dmitrii Altukhov <dm.altukhov@ya.ru>
2022-06-15 18:15:55 +0300

    Remove old commented out config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,12 +40,7 @@ return require('packer').startup(function(use)
 
   use {
     'kyazdani42/nvim-web-devicons',
-    config = function() require('nvim-web-devicons').setup {
-      -- override = {
-      --   zsh = { icon = "", color = "#428850", cterm_color = "65", name = "Zsh" },
-      --   default = true,
-      -- }
-    }
+    config = function() require('nvim-web-devicons').setup {}
     end
   }
   use {
