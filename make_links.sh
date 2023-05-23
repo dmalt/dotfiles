1fad560494f8357ea68fde86ed9ebd8914995dae
Dmitrii Altukhov <dm.altukhov@ya.ru>
2023-05-23 22:52:58 +0200

    update tmux dest location

--- make_links.sh
+++ make_links.sh
@@ -1,23 +1,26 @@
-export DOTFILES=$HOME/.dotfiles
+#!/usr/bin/env sh
+export DOTFILES="$HOME/.dotfiles"
 
-
-ln -sfv $DOTFILES/gitconfig             $HOME/.gitconfig
-ln -sfv $DOTFILES/zshrc                 $HOME/.zshrc
-ln -sfv $DOTFILES/pure_dmalt.zsh-theme  $HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme
-ln -sfv $DOTFILES/tmux.conf             $HOME/.tmux.conf
-ln -sfv $DOTFILES/xinputrc              $HOME/.xinputrc
-ln -sfv $DOTFILES/nvim/init.vim         $HOME/.vimrc
-ln -sfv $DOTFILES/zlogin                $HOME/.zlogin
+ln -sfv "$DOTFILES/gitconfig"             "$HOME/.gitconfig"
+ln -sfv "$DOTFILES/zshrc"                 "$HOME/.zshrc"
+ln -sfv "$DOTFILES/pure_dmalt.zsh-theme"  "$HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme"
+ln -sfv "$DOTFILES/xinputrc"              "$HOME/.xinputrc"
+ln -sfv "$DOTFILES/zlogin"                "$HOME/.zlogin"
 
 if [ -L "$HOME/.config/nvim" ] || [ -d "$HOME/.config/nvim" ]
 then
-    rm -r $HOME/.config/nvim
+    rm -r "$HOME/.config/nvim"
 fi
-ln -sfv $DOTFILES/nvim $HOME/.config/nvim
+ln -sfv "$DOTFILES/nvim" "$HOME/.config/nvim"
 
+if [ -L "$HOME/.config/tmux" ] || [ -d "$HOME/.config/tmux" ]
+then
+    rm -r "$HOME/.config/tmux"
+fi
+ln -sfv "$DOTFILES/tmux.conf"             "$HOME/.config/tmux/tmux.conf"
 
 if [ -L "$HOME/.config/ranger" ] || [ -d "$HOME/.config/ranger" ]
 then
-    rm -r $HOME/.config/ranger
+    rm -r "$HOME/.config/ranger"
 fi
-ln -sfv "$DOTFILES/ranger"              $HOME/.config/ranger
+ln -sfv "$DOTFILES/ranger"              "$HOME/.config/ranger"
