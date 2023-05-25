7d430ca12b25bbc3d26d6ebaef2dc5d4180e157d
Dmitrii Altukhov <dm.altukhov@ya.ru>
2023-05-25 16:36:07 +0200

    obsidian.nvim: dont change note name

--- nvim/lua/config/obsidian.lua
+++ nvim/lua/config/obsidian.lua
@@ -9,28 +9,28 @@ function M.setup()
     completion = {nvim_cmp = true},
 
     -- Optional, customize how names/IDs for new notes are created.
-    note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-      local suffix = ""
-      if title ~= nil then
-        -- If title is given, transform it into valid file name.
-        suffix = title:gsub(" ", "_"):gsub("[^A-Za-z0-9_]", ""):lower()
-      else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
-        end
-      end
-      return suffix
-    end,
+    -- note_id_func = function(title)
+    --   -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    --   -- In this case a note with the title 'My new note' will given an ID that looks
+    --   -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    --   local suffix = ""
+    --   if title ~= nil then
+    --     -- If title is given, transform it into valid file name.
+    --     suffix = title:gsub(" ", "_"):gsub("[^A-Za-z0-9_]", ""):lower()
+    --   else
+    --     -- If title is nil, just add 4 random uppercase letters to the suffix.
+    --     for _ = 1, 4 do
+    --       suffix = suffix .. string.char(math.random(65, 90))
+    --     end
+    --   end
+    --   return suffix
+    -- end,
 
     -- Optional, set to true if you don't want Obsidian to manage frontmatter.
     disable_frontmatter = true,
 
     -- Optional, for templates (see below).
-    templates = { subdir = "templates", date_format = "%Y-%m-%d-%a", time_format = "%H:%M" },
+    templates = { subdir = "templates", date_format = "%Y-%m-%d", time_format = "%H:%M" },
 
     -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
     -- URL it will be ignored but you can customize this behavior here.
