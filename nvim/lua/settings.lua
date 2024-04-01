f13094531a5d59a2b9ae0824b7ec483a3daf249c
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2024-04-01 20:21:42 +0200

    update settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,6 +1,6 @@
-local set = vim.opt  ---@diagnostic disable-line: undefined-global
-local g = vim.g  ---@diagnostic disable-line: undefined-global
-Line_length = 99
+local set = vim.opt
+local g = vim.g
+Line_length = 88
 
 set.tabstop = 4
 set.shiftwidth = 4
@@ -16,13 +16,13 @@ set.clipboard = "unnamed,unnamedplus"
 set.splitbelow = true
 set.splitright = true
 set.termguicolors = true
-set.diffopt = vim.opt.diffopt + "vertical"  ---@diagnostic disable-line
+set.diffopt = vim.opt.diffopt + "vertical"
 
-set.number = true
-set.relativenumber = true
+set.number = true  -- ks
+set.relativenumber = true  -- ks
 set.cursorline = true
 set.list = true  -- enable explicit chars for endlines and such
-set.listchars = {eol = '󰌑', tab = '>-'}
+set.listchars = {eol = '󰌑', tab = '>-', trail = '·'}
 set.foldmethod = "marker"
 
 set.expandtab = true
@@ -33,6 +33,6 @@ set.autoindent = true
 set.colorcolumn = { tostring(Line_length), }
 set.timeoutlen=500
 set.updatetime=300  -- to trigger `vim.diagnostic.open_float` in autocmd in a reasonable time
-
-g.python_host_prog='/usr/bin/python2'
+g.python_host_prog='/opt/homebrew/bin/python3'
 g.python3_host_prog='/opt/homebrew/bin/python3'
+g.snippets = "luasnip"
