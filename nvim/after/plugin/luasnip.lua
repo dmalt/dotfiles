41cdc07716d8cd2098b2e694b9e09ca0b04ce295
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2024-07-07 10:46:04 +0200

    add 'now' snippet

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -64,26 +64,11 @@ local conds_expand = require 'luasnip.extras.conditions.expand'
 local function copy(args)
   return args[1]
 end
-
 ls.add_snippets('all', {
-  -- trigger is `fn`, second argument to snippet-constructor are the nodes to insert into the buffer on expansion.
-  s('fn', {
-    -- Simple static text.
-    t '//Parameters: ',
-    -- function, first parameter is the function, second the Placeholders
-    -- whose text it gets as input.
-    f(copy, 2),
-    t { '', 'function ' },
-    -- Placeholder/Insert.
-    i(1),
-    t '(',
-    -- Placeholder with initial text.
-    i(2, 'int foo'),
-    -- Linebreak
-    t { ') {', '\t' },
-    -- Last Placeholder, exit Point of the snippet.
-    i(0),
-    t { '', '}' },
+  s('now', {
+    f(function(args, snip, user_arg_1)
+      return vim.fn.trim(vim.fn.system 'date +%Y-%m-%dT%H:%M')
+    end, {}),
   }),
 })
 
