20f8be89118b0935932bd1bd5e26dc2bb8b9635e
Dmitrii Altukhov <dmitrii.altukhov@creativefabrica.com>
2026-01-22 11:23:36 +0100

    remove obsolete stuff from make_links.sh

--- make_links.sh
+++ make_links.sh
@@ -3,9 +3,9 @@ export DOTFILES="$HOME/.dotfiles"
 
 ln -sfv "$DOTFILES/gitconfig"             "$HOME/.gitconfig"
 ln -sfv "$DOTFILES/zshrc"                 "$HOME/.zshrc"
-ln -sfv "$DOTFILES/pure_dmalt.zsh-theme"  "$HOME/.oh-my-zsh/themes/pure_dmalt.zsh-theme"
-ln -sfv "$DOTFILES/xinputrc"              "$HOME/.xinputrc"
 ln -sfv "$DOTFILES/zlogin"                "$HOME/.zlogin"
+
+mkdir -p $HOME/.config
 
 if [ -L "$HOME/.config/nvim" ] || [ -d "$HOME/.config/nvim" ]
 then
@@ -19,9 +19,3 @@ then
 fi
 mkdir "$HOME/.config/tmux"
 ln -sfv "$DOTFILES/tmux.conf"             "$HOME/.config/tmux/tmux.conf"
-
-if [ -L "$HOME/.config/ranger" ] || [ -d "$HOME/.config/ranger" ]
-then
-    rm -r "$HOME/.config/ranger"
-fi
-ln -sfv "$DOTFILES/ranger"              "$HOME/.config/ranger"
